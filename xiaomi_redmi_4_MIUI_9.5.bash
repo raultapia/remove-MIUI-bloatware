15187dea52daca12782a5b1e2bdcfc17cad77ea9
rautaplop <47368172+rautaplop@users.noreply.github.com>
2019-05-04 21:26:13 +0200

    Update and rename remove_bloatware_xiaomi_redmi_4_MIUI_9.5.bash to xiaomi_redmi_4_MIUI_9.5.bash

--- xiaomi_redmi_4_MIUI_9.5.bash
+++ xiaomi_redmi_4_MIUI_9.5.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-### remove_bloatware_xiaomi_redmi_4_MIUI_9.5.bash
+### xiaomi_redmi_4_MIUI_9.5.bash
 ### github.com/rautaplop
 
 ### This file is used for removing bloatware in Xiaomi Redmi 4 with MIUI Global 9.5 Stable
